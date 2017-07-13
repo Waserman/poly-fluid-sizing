cdb566adba0a76169eed6263e3d8a6e31b620d0a
Avi Waserman <aviw@wobi.co.il>
2017-07-13 18:59:01 +0300

    Project scaffolding - WIP

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data() {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import './sass/base';
+
+html {
+  box-sizing: border-box;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+
+*:not(html):not(head):not(script) {
+  box-sizing: inherit;
+
+  &:after,
+  &:before {
+    box-sizing: inherit;
+  }
+}
+
+html,
+body {
+  width: 100%;
+  height: 100%;
+  font-size: 100%;
+}
+
+body {
+  position: relative;
+}
+
+</style>
+
