f926e91e119b398656e78b94c94717829cd8b4cc
Waserman <waserman2302@gmail.com>
2017-07-14 12:35:16 +0300

    WIP

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
     <div>
-
+      <p id="fontsize">this is the font size: {{ fontSize }}</p>
+      the view port is: {{ vw }}
     </div>
   </div>
 </template>
@@ -11,40 +12,47 @@ export default {
   name: 'app',
   data() {
     return {
+      fontSize: '',
+      vw: ''
     }
+  },
+  mounted () {
+    window.addEventListener('resize', (e) => {
+      this.$nextTick(() => {
+        this.vw = document.documentElement.clientWidth
+        let paragraph = document.querySelector('#fontsize')
+        let font_size = window.getComputedStyle(paragraph, null).getPropertyValue('font-size')
+
+        this.fontSize = parseFloat(font_size) + 'px'
+
+      })
+    })
   }
 }
 </script>
 
-<style lang="scss" scoped>
-@import './sass/base';
+<style lang="scss">
+  @import './sass/base';
+    html {
+        box-sizing: border-box;
+        height: 100%;
+        margin:0;
+        padding: 0;
+    }
+    *:not(html):not(head):not(script) {
+        box-sizing: inherit;
+        &:after, &:before {
+            box-sizing: inherit;
+        }
+    }
+    html, body {
+        width: 100%;
+        height: 100%;
+        padding: 30px;
+    }
 
-html {
-  box-sizing: border-box;
-  height: 100%;
-  margin: 0;
-  padding: 0;
-}
-
-*:not(html):not(head):not(script) {
-  box-sizing: inherit;
-
-  &:after,
-  &:before {
-    box-sizing: inherit;
-  }
-}
-
-html,
-body {
-  width: 100%;
-  height: 100%;
-  font-size: 100%;
-}
-
-body {
-  position: relative;
-}
-
+    #fontsize {
+      @include poly-fluid-sizing('font-size', (640px: 24px, 1920px: 40px)); 
+    }
 </style>
 
